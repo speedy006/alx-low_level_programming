ca8d8fa8214cc9dba4801bd35171e6b405559e0c
speedy006 <joemelvin385@gmail.com>
2023-09-08 05:04:57 -0700

    11 corrected01

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,22 +9,20 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x <= 7; x++)
 	{
-		for (y = 0; y < 10; y++)
+		for (y = 0; y <= 8; y++)
 		{
-			for (z = 0; z < 10; z++)
+			for (z = 0; z <= 9; z++)
 			{
-				if (x != y && y != z && x != z)
+				putchar(x + '0');
+				putchar(y + '0');
+				putchar(z + '0');
+
+				if (!(x == 7 && y == 8 && z == 9))
 				{
-					putchar(x + '0');
-					putchar(y + '0');
-					putchar(z + '0');
-					if (x != 7 || y != 8 || z != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
