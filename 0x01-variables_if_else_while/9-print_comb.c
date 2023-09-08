e1475aebe638debe8d28fde904ed8c7ecb7a3042
speedy006 <joemelvin385@gmail.com>
2023-09-08 04:13:45 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - printing possible combinations
+* Return: 0
+*/
+
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		putchar(d + '0');
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
