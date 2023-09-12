a9e20922aa26f2e1fe3731ab3b47e08a75059696
speedy006 <joemelvin385@gmail.com>
2023-09-12 04:48:14 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - printing
+ * @x - character
+ * Return: 0
+*/
+
+int _islower(int x)
+{
+	if (x >= 'a' && x <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
