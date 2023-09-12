cea0af3936c2146a5ed10a67efe7540d80be4990
speedy006 <joemelvin385@gmail.com>
2023-09-12 05:36:49 -0700

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - printing
+*/
+
+void times_table(void)
+{
+int i, j, result;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+result = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+if (result >= 10)
+_putchar(result / 10 + '0');
+else
+_putchar(' ');
+}
+_putchar(result % 10 + '0');
+}
+_putchar('\n');
+}
+}
