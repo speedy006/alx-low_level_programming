a691baa6906bb1db8b7c16c2efb1d7513d460e7c
speedy006 <joemelvin385@gmail.com>
2023-09-14 04:49:05 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - printing numbers
+ */
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != '2' || x != '4')
+		_putchar(x);
+	}
+	_putchar('\n');
+}
