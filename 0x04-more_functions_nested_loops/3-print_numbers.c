0a02e609d137f0dafc9d13fccff09d7735c5a276
speedy006 <joemelvin385@gmail.com>
2023-09-14 04:44:37 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers
+ */
+
+void print_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
