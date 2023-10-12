01575989ad10b54543a8f9aa1fc454c18ebe6570
speedy006 <joemelvin385@gmail.com>
2023-10-12 04:07:44 -0700

    0 test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,11 +13,6 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int sum = 0;
 	unsigned int x;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-
 	va_start(func, n);
 
 	for (x = 0; x < n; x++)
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _VARIADIC_FUNCTIONS_H
-#define _VARIADIC_FUCTIONS_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
