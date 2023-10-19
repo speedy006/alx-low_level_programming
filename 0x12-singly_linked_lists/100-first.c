586413c4f72703c80c698ac8fa49c966ab7ddd4f
speedy006 <joemelvin385@gmail.com>
2023-10-19 07:17:49 -0700

    5 tested

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * premain - function called before main
+*/
+
+void __attribute__((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
